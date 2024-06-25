81c07808c3d06d61e66d529950bab5b583f277be
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-06-25 21:54:45 +0300

    Python-p3-functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+# Function calls
+greet_programmer()
+greet("Naureen")
+greet_with_default("Sunny")
+add(1, 2)
+halve("two")
+
